9aa9c52773f6e3247829639777cd3903e7c46852
Aniket Veer Koppaka <146194681+Aniketkoppaka@users.noreply.github.com>
2025-06-19 02:30:06 +0530

    Add files via upload

---
+++ QR Code Project/index.js
@@ -0,0 +1,29 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image'
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {
+      message: 'Please enter a URL to generate a QR code:',
+      name: 'URL',
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile("URL.txt", url, (err) => {
+          if (err) throw err;
+          console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.error("Prompt couldn't be rendered in the current environment");
+    } else {
+      console.error("Something else went wrong");
+    }
+  });
+
